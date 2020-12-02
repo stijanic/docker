7ce3f3607bf87fda623587349d972e33e7670490
stijanic <stijanic@yahoo.fr>
2020-12-02 02:12:32 +0100

    Cleaning

--- clojure/1.10.1/_Clojure/test/src/core.clj
+++ clojure/1.10.1/_Clojure/test/src/core.clj
@@ -6,26 +6,30 @@
 ;; M-x package-install (srefactor, elisp-format, elisp-lint)
 ;; M-x magit-status (? - help)
 
+;; ---------------------------
 ;; namespace
+;; ---------------------------
 (ns core 
   (:require [clojure.string :as string]) 
   (:gen-class))
+;; ---------------------------
 
+;; ---------------------------
 ;; types
+;; ---------------------------
 (def aString "This is my 1st string")
 (def aLong 15)
-(def aDouble 1.234)
-
-(println "----- Start -----")
-(println (format "pos? %b" (pos? 15)))
-(println (format "odd? %b" (odd? 15)))
-(println (format "even? %b" (even? 15)))
-(println (format "string? %b" (string? 15)))
-(println (format "integer? %b" (integer? 15)))
-(println (format "number? %b" (number? 15)))
-(println (format "float? %b" (float? 15)))
+;; ---------------------------
+(println "----- Boolean -----")
+(println (format "pos? %b" (pos? aLong)))
+(println (format "odd? %b" (odd? aLong)))
+(println (format "even? %b" (even? aLong)))
+(println (format "string? %b" (string? aLong)))
+(println (format "integer? %b" (integer? aLong)))
+(println (format "number? %b" (number? aLong)))
+(println (format "float? %b" (float? aLong)))
 (println (format "nil? %b" (nil? aLong)))
-(println "------------------")
+(println "------ String -----")
 (println (format "blank? %b" (string/blank? aString)))
 (println (format "includes? %b" (string/includes? aString "ll")))
 (println (format "index-of %d" (string/index-of aString "ll")))
@@ -35,16 +39,19 @@
 (println (format "replace %s" (string/replace "I am 42 " #"42" "43")))
 (println (format "upper-case %s" (string/upper-case aString)))
 (println (format "lower-case %s" (string/lower-case aString)))
-(println "------------------")
+(println "-------------------")
+;; ---------------------------
 
-
+;; ---------------------------
 ;; functions
-(defn -main "I don't do a whole lot ... yet." [& args] 
+;; ---------------------------
+(defn -main "I don't do a whole lot ... yet."
+  [& args]
   (println "I'm a little teapot!" (first args)))
-
-(defn foo "I don't do a whole lot." [x] 
-  (println x "Hello, World!"))
-
+(defn foo "I don't do a whole lot."
+  [x]
+  (println (format "Hello, %s!" x)))
+;; ---------------------------
 (-main 1 2 3 4)
-(-main "cool")
-(println "------------------")
+(foo "World")
+;; ---------------------------
