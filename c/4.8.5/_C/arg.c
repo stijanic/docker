459a0b68e0c5a859e3c2e1e667947d478519ddf2
stijanic <stijanic@yahoo.fr>
2020-12-26 19:34:13 +0100

    Good

--- c/4.8.5/_C/arg.c
+++ c/4.8.5/_C/arg.c
@@ -4,7 +4,7 @@ int main( int argc, char *argv[] )
 {
    if( argc == 2 )
    {
-      printf("The argument supplied is %s\n", argv[1]);
+     printf("The argument supplied is %s\n", argv[1]);
    }
    else if( argc > 2 )
    {
