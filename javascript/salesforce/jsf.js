d22e95374be2f05e5a2350f26a3fc60e1f34d7e9
stijanic <stijanic@yahoo.fr>
2021-02-03 01:17:27 +0100

    All nice and clean

--- javascript/salesforce/jsf.js
+++ javascript/salesforce/jsf.js
@@ -1,6 +1,6 @@
 var jsforce = require('jsforce');
 var conn = new jsforce.Connection();
-conn.login('stijanic@yahoo.fr', 'xyz123SFcBV79sywNt77o0OXo26ZHvbqL', function(err, res) {
+conn.login('stijanic@yahoo.fr', 'xyz123SF*wNy8MDEqugokvsULTUJt7K4u7', function(err, res) {
   if (err) { return console.error(err); }
   conn.query('SELECT Id, Name FROM Contact', function(err, res) {
     if (err) { return console.error(err); }
