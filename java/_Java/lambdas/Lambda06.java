e21d847fecb66b5fcc4fd1b03ef3f21690cad245
stijanic <stijanic@yahoo.fr>
2021-02-15 07:14:41 +0100

    More lambdas

--- java/_Java/lambdas/Lambda06.java
+++ java/_Java/lambdas/Lambda06.java
@@ -1,8 +1,5 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public class Lambda06 {
 	public static void main(String args[]) {
